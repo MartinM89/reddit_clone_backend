e126c14a8e0a21a1b719b7e7f1e0ca5b2965c11c
Martin <saadv89@gmail.com>
2025-01-20 19:49:41 +0100

    added simple methods to delete, update, and add posts

--- Controllers/PostgresPostController.cs
+++ Controllers/PostgresPostController.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
 
-// namespace test_project.Controllers;
-
 [ApiController]
 [Route("api/[controller]")]
 public class PostgresPostController : ControllerBase
@@ -12,12 +10,12 @@ public class PostgresPostController : ControllerBase
     {
         try
         {
-            var user = FetchData.GetPost(id);
-            return Ok(user);
+            var post = FetchData.GetPost(id);
+            return Ok(post);
         }
         catch (Exception ex)
         {
-            return BadRequest(new { Message = ex.Message });
+            return BadRequest(new { message = $"Couldn't get post. {ex.Message}" });
         }
     }
 
@@ -32,10 +30,57 @@ public class PostgresPostController : ControllerBase
         }
         catch (Exception ex)
         {
-            return BadRequest(new { Message = ex.Message });
+            return BadRequest(new { message = $"Couldn't get all posts. {ex.Message}" });
+        }
+    }
+
+    [HttpGet]
+    [Route("addpost")]
+    public IActionResult AddPost(string title)
+    {
+        try
+        {
+            FetchData.AddPost(title);
+            return Ok(new { Message = "Post added" });
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new { message = $"Couldn't add post. {ex.Message}" });
+        }
+    }
+
+    [HttpGet]
+    [Route("deletepost")]
+    public IActionResult DeletePost(int id)
+    {
+        try
+        {
+            FetchData.DeletePost(id);
+            return Ok(new { Message = "Post deleted" });
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new { message = $"Couldn't delete post. {ex.Message}" });
+        }
+    }
+
+    [HttpGet]
+    [Route("updatepost")]
+    public IActionResult UpdatePost(int id, string title)
+    {
+        try
+        {
+            FetchData.UpdatePost(id, title);
+            return Ok(new { Message = "Post updated" });
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new { message = $"Couldn't update post. {ex.Message}" });
         }
     }
 }
 
 // http://localhost:5049/api/postgrespost/getpost?id=1
 // http://localhost:5049/api/postgrespost/getallposts
+// http://localhost:5049/api/postgrespost/deletepost?id=1
+// http://localhost:5049/api/postgrespost/addpost?title=New_Post
--- FetchData.cs
+++ FetchData.cs
@@ -39,4 +39,80 @@ public class FetchData
             throw new Exception("Could not fetch posts");
         }
     }
+
+    public static void AddPost(string title)
+    {
+        Post post = new Post
+        {
+            Rating = 0,
+            Title = title,
+            Submitted = DateTime.Now.ToString(),
+            Username = "fresh_baker",
+            SubReddit = "r/programming",
+            CommentAmount = 5,
+        };
+
+        try
+        {
+            using var db = new AppContext();
+
+            db.Posts.Add(post);
+            db.SaveChanges();
+        }
+        catch (Exception)
+        {
+            throw new Exception("Could not delete post");
+        }
+    }
+
+    public static void DeletePost(int id)
+    {
+        try
+        {
+            using var db = new AppContext();
+
+            var post = db.Posts.FirstOrDefault(p => p.Id == id);
+
+            if (post == null)
+            {
+                throw new Exception("Post not found");
+            }
+
+            db.Posts.Remove(post);
+        }
+        catch (Exception)
+        {
+            throw new Exception("Could not delete post");
+        }
+    }
+
+    // var post = new Post { Id = id };
+    // db.Posts.Attach(post);
+    // db.Posts.Remove(post);
+
+    public static void UpdatePost(int id, string title)
+    {
+        Post post = new Post
+        {
+            Id = id,
+            Rating = 0,
+            Title = title,
+            Submitted = DateTime.Now.ToString(),
+            Username = "fresh_baker",
+            SubReddit = "r/programming",
+            CommentAmount = 5,
+        };
+
+        try
+        {
+            using var db = new AppContext();
+
+            db.Posts.Update(post);
+            db.SaveChanges();
+        }
+        catch (Exception)
+        {
+            throw new Exception("Could not delete post");
+        }
+    }
 }
